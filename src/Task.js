f5bb86d001d43eeefe41b356acb8cf549961f5df
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-09-13 13:22:55 +0200

    fix time display

--- src/Task.js
+++ src/Task.js
@@ -12,9 +12,9 @@ class Task extends Component {
 
         <div className="time-info">
           <time className="day">{this.props.task.date.format('ddd')}</time>
-          <time className="time">{this.props.task.starttime.format('hh:mm')}</time>
+          <time className="time">{this.props.task.starttime.format('HH:mm')}</time>
           &ndash;
-          <time className="time">{this.props.task.endtime.format('hh:mm')}</time>
+          <time className="time">{this.props.task.endtime.format('HH:mm')}</time>
         </div>
         
         <p className="tooltip">
