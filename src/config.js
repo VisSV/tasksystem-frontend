244e68e395bdb05fd65384cdf1f5dacb97e16f24
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-09-14 15:45:25 +0200

    fix color map

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ var config = {
   //hostname: 'localhost:8000',
   protocol: 'https',
   hostname: 'sv-task-server.herokuapp.com',
-  colorScale: ['#fbb4ae','#b3cde3','#ccebc5','#decbe4','#fed9a6','#ffffcc','#e5d8bd','#fddaec'] // colorbrewer pastel1
+  colorScale: ['', '#fbb4ae','#b3cde3','#ccebc5','#decbe4','#fed9a6','#ffffcc','#e5d8bd','#fddaec'] // colorbrewer pastel1
 };
 
 config['httpaddr'] = config.protocol + '://' + config.hostname;
