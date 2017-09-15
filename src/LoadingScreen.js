8bf319c066cb7ff449baf94ad39b305f9b64d595
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-09-15 09:30:10 +0200

    main application loading screen

--- src/LoadingScreen.js
+++ src/LoadingScreen.js
@@ -4,7 +4,22 @@ import React, { Component } from 'react';
 class LoadingScreen extends Component {
   render() {
     return (
-      <div className="loading-spinner">
+      <div className="LoadingScreen">
+        <div className="spinner">
+          <div className="spinner-stage-1"></div>
+          <div className="spinner-stage-2"></div>
+          <div className="spinner-stage-3"></div>
+          <div className="spinner-stage-4"></div>
+          <div className="spinner-stage-5"></div>
+          <div className="spinner-stage-6"></div>
+          <div className="spinner-stage-7"></div>
+          <div className="spinner-stage-8"></div>
+          <div className="spinner-stage-9"></div>
+          <div className="spinner-stage-10"></div>
+          <div className="spinner-stage-11"></div>
+          <div className="spinner-stage-12"></div>
+        </div>
+        <p className="spinner-text">Loading...</p>
       </div>
     );
   }
