c8bd0e867ecbf4d75880d0944d3628ea695eecb8
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-09-17 11:32:35 +0200

    wrong config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 
 var config = {
-  protocol: 'http',
-  hostname: 'localhost:8000',
-  //protocol: 'https',
-  //hostname: 'sv-task-server.herokuapp.com',
+  //protocol: 'http',
+  //hostname: 'localhost:8000',
+  protocol: 'https',
+  hostname: 'sv-task-server.herokuapp.com',
   colorScale: ['', '#fbb4ae','#b3cde3','#ccebc5','#decbe4','#fed9a6','#ffffcc','#e5d8bd','#fddaec'] // colorbrewer pastel1
 };
 
