79cef526643aa17f6a7d0cffa94b1689a8f9ddbf
Ubuntu <ubuntu@ip-172-31-14-94.us-west-2.compute.internal>
2020-09-18 01:38:03 +0000

    made some small changes

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 
 var config = {
-  // protocol: 'http',
-  // hostname: 'localhost:8000',
-  protocol: 'https',
-  hostname: 'sv-task-system.herokuapp.com',
+  protocol: 'http',
+  hostname: 'localhost:8000',
+  //protocol: 'https',
+  //hostname: 'sv-task-system.herokuapp.com',
   colorScale: ['', '#f7cbd7','#b3cde3','#ccebc5','#decbe4','#fed9a6','#ffffcc','#bde4e5','#dbdbdb'] // colorbrewer pastel1
 };
 
