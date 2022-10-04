54f9cc2772bb4ae851fe8621a10a221649973513
Juan Trelles <juantrelles@gmail.com>
2022-10-03 22:59:44 -0500

    update hours in limit panel

--- src/LimitPanel.js
+++ src/LimitPanel.js
@@ -1,61 +1,76 @@
-import React, { Component } from 'react';
-import _ from 'lodash';
-import taskHours from './util';
+import React, { Component } from "react";
+import _ from "lodash";
+import taskHours from "./util";
 
 const groupLimits = {
-  1: [0, 6],
-  2: [0, 6],
-  3: [0, 6],
-  4: [0, 6],
-  5: [0, 4],
-  7: [0, 0]
-}
+  1: [0, 14],
+  2: [0, 14],
+  3: [0, 10],
+  4: [0, 14],
+  5: [0, 10],
+  6: [0, 6],
+  7: [0, 1],
+};
 const hourLimit = 14;
 
 var defaultGroups = {};
-_.keys(groupLimits).forEach(function(k) {
+_.keys(groupLimits).forEach(function (k) {
   defaultGroups[k] = [];
 });
 
-// Limits are not explicitly enforced. The old system did this so 
+// Limits are not explicitly enforced. The old system did this so
 // this one does the same
 class LimitPanel extends Component {
   render() {
     var tasks = _.values(this.props.tasks.val());
-    var groupedTasks = _.groupBy(_.values(tasks), 'category');
+    var groupedTasks = _.groupBy(_.values(tasks), "category");
     groupedTasks = Object.assign({}, defaultGroups, groupedTasks);
-    var groupHours = _.mapValues(groupedTasks, function(group) {
-      return _.reduce(group, function(ttl, task) {return ttl + taskHours(task);}, 0);
+    var groupHours = _.mapValues(groupedTasks, function (group) {
+      return _.reduce(
+        group,
+        function (ttl, task) {
+          return ttl + taskHours(task);
+        },
+        0
+      );
     });
     var groups = _.sortBy(_.keys(groupLimits));
-    var categoryCounts = groups.map(function(g) {
+    var categoryCounts = groups.map(function (g) {
       var classes = "";
-      if(groupHours[g] < groupLimits[g][0] || groupHours[g] > groupLimits[g][1]) {
+      if (
+        groupHours[g] < groupLimits[g][0] ||
+        groupHours[g] > groupLimits[g][1]
+      ) {
         classes = classes + " violation";
       }
       return (
         <li className={classes} key={g}>
           <label>Category {g}</label>
           <div className="count">
-            {groupLimits[g][0]} &le; {groupHours[g].toFixed(2)} &le; {groupLimits[g][1]}
+            {groupLimits[g][0]} &le; {groupHours[g].toFixed(2)} &le;{" "}
+            {groupLimits[g][1]}
           </div>
         </li>
       );
     });
-    var ttlHours = _.reduce(tasks, function(ttl, task) {
-      return ttl + taskHours(task);
-    }, 0);
-    
+    var ttlHours = _.reduce(
+      tasks,
+      function (ttl, task) {
+        return ttl + taskHours(task);
+      },
+      0
+    );
+
     return (
       <div className="LimitPanel">
         <h2>Limits</h2>
         <p className="info-text">
-          Please pay attention to any red lines. If you take too many tasks
-          of a certain type then the SV chairs will remove those at random.
+          Please pay attention to any red lines. If you take too many tasks of a
+          certain type then the SV chairs will remove those at random.
         </p>
         <ul>
           {categoryCounts}
-          <li className={ttlHours < hourLimit ? 'violation' : ''}>
+          <li className={ttlHours < hourLimit ? "violation" : ""}>
             <label>Total hours</label>
             <div className="count">
               {ttlHours.toFixed(2)} / {hourLimit}
@@ -68,4 +83,3 @@ class LimitPanel extends Component {
 }
 
 export default LimitPanel;
-
