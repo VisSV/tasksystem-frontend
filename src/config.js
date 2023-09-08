5b3b98b0970651458e597d10c24f2a900e5b757a
Kun-Ting Chen <kunting.chen01@gmail.com>
2023-09-08 22:11:23 +0930

    Update staging server URL

--- src/config.js
+++ src/config.js
@@ -1,9 +1,17 @@
 var config = {
   //protocol: 'https',
   //hostname: '54.174.247.37:8000',
-  protocol: "https",
+
+  protocol: 'http',
+  hostname: 'creststudy1.services.adelaide.edu.au:8443',
+
+  //dev: use http
+  // protocol: "http",
   // hostname: "localhost:8000",
-  hostname: "sv-task-system.herokuapp.com",
+
+  //prod: use https
+  // protocol: "https",
+  // hostname: "sv-task-system.herokuapp.com",
   colorScale: [
     "",
     "#f7cbd7",
@@ -18,6 +26,10 @@ var config = {
 };
 
 config["httpaddr"] = config.protocol + "://" + config.hostname;
-config["wsaddr"] = "wss://" + config.hostname;
+//dev: use ws
+// config["wsaddr"] = "ws://" + config.hostname;
+
+//prod: use wss
+config["wsaddr"] = "ws://" + config.hostname;
 
 export default config;
