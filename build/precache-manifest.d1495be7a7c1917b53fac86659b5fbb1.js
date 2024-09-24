6bbb4e6a38bd2decab9d8ff156f813f8caf2e88f
kawilliams <kawilliams@davidson.edu>
2024-09-24 17:22:30 -0400

    Prepare for Heroku devlopment

---
+++ build/precache-manifest.d1495be7a7c1917b53fac86659b5fbb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9255eac6993aafcc58292e3747c7da2",
+    "url": "/tasksystem/index.html"
+  },
+  {
+    "revision": "48fab23657c4cf3dc181",
+    "url": "/tasksystem/static/css/main.08af2cf5.chunk.css"
+  },
+  {
+    "revision": "14a47173807a1c464d86",
+    "url": "/tasksystem/static/js/2.e680269b.chunk.js"
+  },
+  {
+    "revision": "516b28df362d3571e58b9d26278408b5",
+    "url": "/tasksystem/static/js/2.e680269b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48fab23657c4cf3dc181",
+    "url": "/tasksystem/static/js/main.292c1acd.chunk.js"
+  },
+  {
+    "revision": "84be56ae56dc3e1c01de",
+    "url": "/tasksystem/static/js/runtime-main.4f2d62bc.js"
+  }
+]);
